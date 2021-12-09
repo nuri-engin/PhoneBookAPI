0af3e7be185235e3676235f6cb58bd1e550405f7
Nuri Engin <nuri@engin.web.tr>
2021-12-09 12:37:28 +0300

    ✅ : Adapting the test cases to work with a 'contact' entity.

--- tests/unit/models/contact.model.test.js
+++ tests/unit/models/contact.model.test.js
@@ -9,6 +9,10 @@ describe('Contact model', () => {
         name: faker.name.findName(),
         email: faker.internet.email().toLowerCase(),
         address: faker.phone.phoneNumber(),
+        workNumber: faker.phone.phoneNumber(),
+        homeNumber: faker.phone.phoneNumber(),
+        mobileNumber: faker.phone.phoneNumber(),
+        otherNumber: faker.phone.phoneNumber(),
       };
     });
 
@@ -19,6 +23,24 @@ describe('Contact model', () => {
     test('should throw a validation error if email is invalid', async () => {
       newContact.email = 'invalidEmail';
       await expect(new Contact(newContact).validate()).rejects.toThrow();
+    });
+
+    test('should throw a validation error if role is unknown', async () => {
+      newContact.role = 'invalid';
+      await expect(new Contact(newContact).validate()).rejects.toThrow();
+    });
+  });
+
+  describe('Contact toJSON()', () => {
+    test('should not return contact otherNumber when toJSON is called', () => {
+      const newContact = {
+        name: faker.name.findName(),
+        email: faker.internet.email().toLowerCase(),
+        workNumber: faker.phone.phoneNumber(),
+        homeNumber: faker.phone.phoneNumber(),
+        mobileNumber: faker.phone.phoneNumber(),
+      };
+      expect(new Contact(newContact).toJSON()).not.toHaveProperty('otherNumber');
     });
   });
 });
