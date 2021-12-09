32c7f3794c32245b88c5ca2878412bb24365717f
Nuri Engin <nuri@engin.web.tr>
2021-12-09 11:29:18 +0300

    🐛 : Missing registration based fixes applied and now the sample project is up-to-run on dev mode

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,19 +1,16 @@
 const Joi = require('joi');
-const { password, objectId } = require('./custom.validation');
+const { objectId } = require('./custom.validation');
 
 const createUser = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().custom(password),
     name: Joi.string().required(),
-    role: Joi.string().required().valid('user', 'admin'),
   }),
 };
 
 const getUsers = {
   query: Joi.object().keys({
     name: Joi.string(),
-    role: Joi.string(),
     sortBy: Joi.string(),
     limit: Joi.number().integer(),
     page: Joi.number().integer(),
@@ -33,7 +30,6 @@ const updateUser = {
   body: Joi.object()
     .keys({
       email: Joi.string().email(),
-      password: Joi.string().custom(password),
       name: Joi.string(),
     })
     .min(1),
